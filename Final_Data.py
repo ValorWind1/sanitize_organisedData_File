33fa9b65f7ef40626bfddc6bb1b950cd31bd2ea5
ValorWind1 <skyjdani@gmail.com>
2019-05-15 21:55:10 -0400

    ve 1.0

--- Final_Data.py
+++ Final_Data.py
@@ -1,30 +1,23 @@
 """
 
 function to open data ,reads  and separates by commas
+
 """
-def readD():
-    with open("   ")
+def readD(textfile):
 
-with open ("Lewis.txt") as lewis:
-    data = lewis.readline()
-lewis1= data.strip().split(",")
-with open("Max.txt") as max:
-    data= max.readline()
-max1 = data.strip().split(",")
-with open("vettel.txt") as vt:
-    data = vt.readline()
-vt1 = data.strip().split(",")
+    try:
+        with open(textfile) as text_file:
+            data = text_file.readline()
+        return (data.strip().split(","))
+    except IOError as error:
+        print("File Error" +str(error))
 
-
-pure_lewis= []
-pure_max= []
-puer_vt=[]
 
 
 """
 if there are -  . :, in the txt files we change it to : 
-"""
 
+"""
 
 def neat(time_lap):
     if "." in time_lap:
@@ -37,15 +30,22 @@ def neat(time_lap):
 
     return(minutes+":"+seconds)
 
-for i in lewis1:
-    pure_lewis.append(neat(i))
-for i in max1:
-    pure_max.append(neat(i))
-for i in vt1:
-    puer_vt.append(neat(i))
 
 """
-sort functions after organising data
+sort functions after organising data , and print 
 """
-def sort ():
-    for i in
+
+lewis=readD("Lewis.txt")
+
+max=readD("Max.txt")
+
+vtl=readD("vettel.txt")
+
+
+
+print(sorted(set([neat(i) for i in lewis]))[0:3])
+print(sorted(set([neat(i) for i in max]))[0:3])
+print(sorted(set([neat(i) for i in vtl]))[0:3])
+
+
+
